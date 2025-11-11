0a4605b76d5658b7e9e79006e8ff0f333670fda7
mirnafahmy <153024997+mirnafahmy@users.noreply.github.com>
2025-11-11 11:56:05 -0500

    Correct average age calculation for men
    Fix indexing error in average age calculation for men.

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -9,7 +9,7 @@ def calculate_demographic_data(print_data=True):
     race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1])
+    average_age_men = round(df[df['sex'] == ['Male']['age'].mean(), 1])
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = round(df[df['education'] == 'Bachelors'].shape[0] / df.shape[0] * 100, 1)
